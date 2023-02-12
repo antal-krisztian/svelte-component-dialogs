e96e407bbe067e72cd114dec00ae99241799ea0c
Antal Krisztián <antal.krisztian@outlook.com>
2023-02-12 13:39:26 +0200

    upgrade packages; use base as anchor base

--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -1,24 +1,26 @@
+import { base } from '$app/paths';
+
 export interface Page {
 	link: string;
 	name: string;
 }
 
 export const INTRODUCTION_PAGE: Page = {
-	link: '/',
+	link: `${base}/`,
 	name: 'Introduction'
 };
 
 export const GETTING_STARTED_PAGE: Page = {
-	link: 'getting-started',
+	link: `${base}/getting-started`,
 	name: 'Getting Started'
 };
 
 export const API_PAGE: Page = {
-	link: 'api',
+	link: `${base}/api`,
 	name: 'API'
 };
 
 export const EXAMPLES_PAGE: Page = {
-	link: 'examples',
+	link: `${base}/examples`,
 	name: 'Examples'
 };
